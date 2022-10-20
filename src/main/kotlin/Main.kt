4f5c0e9f717281e60d055dbd802347e172568531
Jose Ramos <luislang93@gmail.com>
2022-10-20 16:28:01 -0500

    feat: add `sum` functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,18 @@
 fun main(args: Array<String>) {
     println("Hello World!")
-
-    // Try adding program arguments at Run/Debug configuration
     println("Program arguments: ${args.joinToString()}")
+}
+
+// A function with two Int parameters and Int return type.
+fun sum(a: Int, b: Int): Int {
+    return a + b
+}
+
+/**
+ * A function that returns no meaningful value.
+ *
+ * NOTE: Unit return type can be omitted.
+ **/
+fun printSum(a: Int, b: Int): Unit {
+    println("sum of $a and $b is ${a + b}")
 }
